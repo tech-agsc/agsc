5a01f3484bfe70e42b2636bd5e7cee2efdf764ed
Andres Artunduaga <andres.artunduaga@outlook.com>
2026-02-24 22:42:07 -0500

    actualziar whatsapp link

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -2,6 +2,6 @@ const WHATSAPP_PHONE = "573202386336";
 const WHATSAPP_DEFAULT_MESSAGE =
   "Hola AG Soluciones Contables, deseo agendar una asesoria.";
 
-export const WHATSAPP_URL = `https://api.whatsapp.com/send/?phone=${WHATSAPP_PHONE}&text=${encodeURIComponent(
+export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_PHONE}?text=${encodeURIComponent(
   WHATSAPP_DEFAULT_MESSAGE
-)}&type=phone_number&app_absent=0`;
+)}`;
